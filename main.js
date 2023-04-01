bc1b9ba3ed3e4bdc4f57564704b47f26f3c74c93
arto-savolainen <111137161+arto-savolainen@users.noreply.github.com>
2023-04-01 04:38:35 +0300

    Timers implemented, periodic data fetching works
    Program now fetches data and updates UI every ten minutes, one minute after assumed site update, so at 11 minutes past the hour, 21 minutes past and so on.
    
    Next up: notifications and minimize to tray.

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 const path = require('path')
 const axios = require('axios')
 
-let mainWindow, workerWindow
+const TEN_MINUTES_MS = 10 * 60 * 1000
+let mainWindow
 
-const getData = async () => {
+const fetchData = async () => {
   const alertTreshold = 0.001 // Test value, final value maybe 0.5?
 
   var response
   try {
     response = await axios.get('https://www.ilmatieteenlaitos.fi/revontulet-ja-avaruussaa', {
-      //Query URL without using browser cache
+      // Query URL without using browser cache
       headers: {
         'Cache-Control': 'no-cache',
         'Pragma': 'no-cache',
@@ -29,18 +30,8 @@ const getData = async () => {
   // var time = formatTime(data[data.length - 1][0]) // if we need to display time
   const time = new Date(data[data.length - 1][0]) //temp
   const activity = data[data.length - 1][1]
-  console.log(data[data.length - 1])
+  // console.log(data[data.length - 1])
   console.log(time, activity)
-
-  // does waiting in main process lock up the app?
-  console.log('sleeping for 5 seconds...')
-  await sleep(5000)
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-console.log('done sleeping')
 
   return activity
 
@@ -50,13 +41,13 @@ console.log('done sleeping')
   // }
 }
 
-const sendActivityToMainWindow = async () => {
+const fetchDataAndSendToMainWindow = async () => {
   console.log('update call received!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-  const activity = await getData()
+  const activity = await fetchData()
   mainWindow.webContents.send('update-activity', activity)
 }
 
-const createWindow = async () => {
+const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 300,
     height: 200,
@@ -71,12 +62,12 @@ const createWindow = async () => {
       label: 'File',
       submenu: [
         {
-          click: () => app.quit(),
-          label: 'Quit',
+          click: () => fetchDataAndSendToMainWindow(),
+          label: 'Update'
         },
         {
-          click: () => sendActivityToMainWindow(),
-          label: 'Update'
+          click: () => app.quit(),
+          label: 'Quit',
         }
       ]
     }
@@ -84,17 +75,47 @@ const createWindow = async () => {
   Menu.setApplicationMenu(menu)
 
   mainWindow.loadFile('index.html')
-  
-  setTimeout(sendActivityToMainWindow, 10000)
 }
 
 app.whenReady().then(() => {
   createWindow()
 
-  ipcMain.on('get-data', (event, activity) => {
-    console.log('täällä')
-    // Simple solution with a single value, expand in future
-    mainWindow.webContents.send('update-activity', activity)
+  // When mainWindow has finished loading and is ready to display
+  mainWindow.webContents.once('did-finish-load', () => {
+    console.log('täällä ollaan mainWindow.webContents.once, ääkköset kusee electron konsolissa')
+    // Get activity data and send it to the renderer
+    fetchDataAndSendToMainWindow()
+
+    // The website we get data from updates every ten minutes past the hour. Se we need to set a timer that triggers a data
+    // fetching operation just after that update. However, the site can take a bit of time to update (up to some tens of seconds).
+    // Here we get how many minutes until update happens from the present moment.
+    time = new Date(Date.now())
+    const offsetMinutes = 10 - (time.getMinutes() % 10)
+    // How many seconds to a full minute? By adding this to offsetMinutes we give the site a 1 minute buffer to update
+    const offsetSeconds = 60 - time.getSeconds()
+    console.log('time:', time, 'minutes:', time.getMinutes(), 'offset:', offsetMinutes)
+    // Time in milliseconds until the clock is 11 minutes past, 21 past, etc.
+    const timerMs = offsetMinutes * 60 * 1000 + offsetSeconds * 1000
+
+    // Set timer to trigger data fetching at the right time
+    console.log('starting timer of', timerMs / 1000, 'seconds')
+    setTimeout(() => {
+      // Here time is about 1 minute after assumed site update. Now we set a timer that will run through the lifetime of the program
+      // and call the data fetch function every 10 minutes.
+      setInterval(() => {
+        console.log('interval executing')
+        var t = new Date(Date.now())
+        console.log(`current time before data fetch: ${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}:${t.getMilliseconds()}`)
+        fetchDataAndSendToMainWindow()
+        t = new Date(Date.now())
+        console.log(`current time after data fetch: ${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}:${t.getMilliseconds()}`)
+
+      }, TEN_MINUTES_MS);
+
+      // First datafetch at the assumed site update time, after this fetching will happen at 10 minute intervals
+      fetchDataAndSendToMainWindow()
+      console.log('end of timer, data sent?')
+    }, timerMs);
   })
 
   // macOS stuff
--- preloadMain.js
+++ preloadMain.js
@@ -3,16 +3,3 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('electronAPI', {
   onUpdateActivity: (callback) => ipcRenderer.on('update-activity', callback)
 })
-
-// window.addEventListener('DOMContentLoaded', () => {
-
-
-//   const replaceText = (selector, text) => {
-//     const element = document.getElementById(selector)
-//     if (element) element.innerText = text
-//   }
-
-//   for (const dependency of ['chrome', 'node', 'electron']) {
-//     replaceText(`${dependency}-version`, process.versions[dependency])
-//   }
-// })
